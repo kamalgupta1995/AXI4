9907fe02fd23850acf267d314be5a4e33aa229f6
godavarthi.c <your ACL email id>
2024-08-23 09:03:31 +0530

    updating the agent

--- verification/testbench/axi_master_agent.sv
+++ verification/testbench/axi_master_agent.sv
@@ -5,8 +5,8 @@ class axi_master_agent extends uvm_agent;
   
  // AXI_config m_cfg;        //config DB in case we are using
   
-	axi_master_monitor monh;
-  axi_sequencer seqrh;
+	//axi_master_monitor monh;
+  axi_master_sequencer seqrh;
   axi_master_driver drvh;
   
   function new(string name,uvm_component parent);
@@ -15,14 +15,14 @@ class axi_master_agent extends uvm_agent;
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db #(axi_config)::get(this,"","axi_config",m_cfg))
+/*    if(!uvm_config_db #(axi_config)::get(this,"","axi_config",m_cfg))
       `uvm_fatal("CONFIG","cannot get() m_cfg from uvm_config_db.Have you set() it?") 
       monh=axi_master_monitor::type_id::create("monh",this); 
-   // if(m_cfg.input_agent_is_active==UVM_ACTIVE)  	// this can be used if we are using the config DB
+*/   // if(m_cfg.input_agent_is_active==UVM_ACTIVE)  	// this can be used if we are using the config DB
 	if(get_is_active()==UVM_ACTIVE) 
       begin
         drvh=axi_master_driver::type_id::create("drvh",this);
-        seqrh= axi_sequencer ::type_id::create("seqrh",this);
+        seqrh= axi_master_sequencer ::type_id::create("seqrh",this);
       end
   endfunction
   
@@ -35,5 +35,6 @@ class axi_master_agent extends uvm_agent;
   endfunction
 endclass :axi_master_agent
   
+
 
 
