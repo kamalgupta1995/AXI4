119114079af6e6f8b8210bdf78f9d6eca274730e
godavarthi.c <your ACL email id>
2024-08-23 08:36:46 +0530

    added updated env

--- verification/testbench/axi_env.sv
+++ verification/testbench/axi_env.sv
@@ -1,27 +1,31 @@
 class axi_env extends uvm_env;
   `uvm_component_utils(axi_env)
-
-    axi_slave_monitor slave_monitor;
-    axi_slave_scoreboard slave_scoreboard;
+  
+  axi_master_agent master;
+/*
+    axi_master_monitor master_monitor;
+    axi_scoreboard scoreboard;
     uvm_analysis_port#(axi_transaction) scoreboard_port;
-
+*/
     function new(string name = "axi_env", uvm_component parent);
         super.new(name, parent);
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-
-        slave_monitor = axi_slave_monitor::type_id::create("slave_monitor", this);
+        master= axi_master_agent::type_id::create("master",this);
+/*
+        master_monitor = axi_master_monitor::type_id::create("master_monitor", this);
         if (!uvm_config_db#(uvm_object_wrapper)::get(this, "*", "scoreboard_port", scoreboard_port))
-            `uvm_fatal("NO_SB_PORT", "slave_scoreboard port not defined for axi_env")
+            `uvm_fatal("NO_SB_PORT", "scoreboard port not defined for axi_env")
         
-        slave_scoreboard = axi_slave_scoreboard::type_id::create("slave_scoreboard", this);
-    endfunction
+        scoreboard = axi_scoreboard::type_id::create("scoreboard", this);
+*/
+	endfunction
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        slave_monitor.analysis_port.connect(scoreboard_port);
+        //master_monitor.analysis_port.connect(scoreboard_port);
     endfunction
 
 endclass
