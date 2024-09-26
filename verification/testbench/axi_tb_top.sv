67ae5fdc9f71ec882ae6f6bbdecf5dc74af9c707
AnupK <anup.k@acldigital.com>
2024-09-26 23:24:28 +0530

    removed parameters from DUT instantiation

--- verification/testbench/axi_tb_top.sv
+++ verification/testbench/axi_tb_top.sv
@@ -7,52 +7,17 @@ module axi_tb_top;
 
     bit ACLK=0;
     bit ARESETn=1;
-/*
-    // Parameters
-    parameter DATA_WIDTH = 32;
-    parameter ADDR_WIDTH = 32;
-    parameter ID_WIDTH = 4;
-    parameter DEPTH = 1024;
-    parameter LEN_WIDTH = 8;
-    parameter SIZE_WIDTH = 3;
-    parameter BURST_WIDTH = 2;
-    parameter LOCK_WIDTH = 1;
-    parameter CACHE_WIDTH = 4;
-    parameter PROT_WIDTH = 3;
-    parameter QOS_WIDTH = 4;
-    parameter RESP_WIDTH = 2;
-    localparam START_ADDR = 32'h00000000;
-    localparam END_ADDR = 32'h000003FF;
-*/
+
     // AXI Interface Instantiation
-  axi_if #(.ID_WIDTH      (4   ),
-           .ADDR_WIDTH    (32  ),
-           .LEN_WIDTH     (8   ),
-           .DATA_WIDTH    (32  ),
-           .STRB_WIDTH    (32/8)
-//              .AWUSER_WIDTH  (AXI_AWUSER_WIDTH),
-//              .WUSER_WIDTH   (AXI_WUSER_WIDTH ),
-//              .BUSER_WIDTH   (AXI_BUSER_WIDTH ),
-//              .ARUSER_WIDTH  (AXI_ARUSER_WIDTH),
-//              .RUSER_WIDTH   (AXI_RUSER_WIDTH )
-            ) axi_if_master0(ACLK, ARESETn);    
+  axi_if #(.ID_WIDTH      (`ID_WIDTH ),
+           .ADDR_WIDTH    (`ADDR_WIDTH ),
+           .LEN_WIDTH     (`LEN_WIDTH ),
+           .DATA_WIDTH    (`DATA_WIDTH ),
+           .STRB_WIDTH    (`DATA_WIDTH/8)
+            ) axi_if_master0 (ACLK, ARESETn);    
     
     // Instantiate the DUT
-    axi4_dut #(
-      .DATA_WIDTH(32),
-      .ADDR_WIDTH(32),
-      .ID_WIDTH(4),
-      .DEPTH(1024),
-      .LEN_WIDTH(8),
-      .SIZE_WIDTH(3),
-      .BURST_WIDTH(2),
-      .LOCK_WIDTH(1),
-      .CACHE_WIDTH(4),
-      .PROT_WIDTH(3),
-      .QOS_WIDTH(4),
-      .RESP_WIDTH(2),
-      .START_ADDR(0)
-    ) dut (
+    axi4_dut dut (
         .ACLK(ACLK),
         .ARESETn(ARESETn),
         .MEM_AWID(axi_if_master0.AXI_AWID),
@@ -60,9 +25,9 @@ module axi_tb_top;
         .MEM_AWLEN(axi_if_master0.AXI_AWLEN),
         .MEM_AWSIZE(axi_if_master0.AXI_AWSIZE),
         .MEM_AWBURST(axi_if_master0.AXI_AWBURST),
-        .MEM_AWLOCK(axi_if_master0.AXI_AWLOCK),
         .MEM_AWCACHE(axi_if_master0.AXI_AWCACHE),
         .MEM_AWPROT(axi_if_master0.AXI_AWPROT),
+        .MEM_AWLOCK(axi_if_master0.AXI_AWLOCK),
 //        .MEM_AWQOS(axi_if_master0.AXI_AWQOS),
         .MEM_AWVALID(axi_if_master0.AXI_AWVALID),
         .MEM_AWREADY(axi_if_master0.AXI_AWREADY),
@@ -81,9 +46,9 @@ module axi_tb_top;
         .MEM_ARLEN(axi_if_master0.AXI_ARLEN),
         .MEM_ARSIZE(axi_if_master0.AXI_ARSIZE),
         .MEM_ARBURST(axi_if_master0.AXI_ARBURST),
-        .MEM_ARLOCK(axi_if_master0.AXI_ARLOCK),
         .MEM_ARCACHE(axi_if_master0.AXI_ARCACHE),
         .MEM_ARPROT(axi_if_master0.AXI_ARPROT),
+        .MEM_ARLOCK(axi_if_master0.AXI_ARLOCK),
 //        .MEM_ARQOS(axi_if_master0.AXI_ARQOS),
         .MEM_ARVALID(axi_if_master0.AXI_ARVALID),
         .MEM_ARREADY(axi_if_master0.AXI_ARREADY),
@@ -92,7 +57,7 @@ module axi_tb_top;
         .MEM_RRESP(axi_if_master0.AXI_RRESP),
         .MEM_RLAST(axi_if_master0.AXI_RLAST),
         .MEM_RVALID(axi_if_master0.AXI_RVALID),
-        .MEM_RREADY(axi_if_master0.AXI_ARREADY)
+        .MEM_RREADY(axi_if_master0.AXI_RREADY)
     );
 
     // Clock Generation
@@ -106,18 +71,17 @@ module axi_tb_top;
         #8;
         ARESETn = 0;
         #17;
-      @(posedge axi_if_master0.AXI_ACLK);
+      	@(posedge axi_if_master0.AXI_ACLK);
         ARESETn = 1;
     end
 
     // Set config_db for Interface and run_test method
     initial begin
-		uvm_config_db#(virtual axi_if)::set(null, "*", "axi_vif", axi_if_master0);
+	uvm_config_db#(virtual axi_if)::set(null, "*", "axi_vif", axi_if_master0);
       	run_test("axi_sanity_test");
-      //run_test("axi_read_test");
     end
 
-      initial #10000 $finish;
+    //initial #10000 $finish;
 
     // Waveform dump (edapg)
   /*  initial begin
@@ -125,7 +89,6 @@ module axi_tb_top;
         $dumpfile("axi_tb_top.vcd");
         $dumpvars(0, axi_tb_top);
     end  */
-
        
  initial
    begin
