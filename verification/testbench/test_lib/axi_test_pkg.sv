67b27c12add51deece7cfa63eef4fef95f5ba976
godavarthi.c <your ACL email id>
2024-11-21 14:46:37 +0530

    axi_test_pkg.sv

---
+++ verification/testbench/test_lib/axi_test_pkg.sv
@@ -0,0 +1,16 @@
+package axi_test_pkg;
+
+   `include "uvm_macros.svh"
+    import uvm_pkg::*;
+
+    import axi_pkg::*;
+
+
+    `include "axi_write_test.sv"
+    `include "axi_read_test.sv"
+    `include "axi_MWR_test.sv"
+
+    
+
+endpackage //axi_test_pkg
+
